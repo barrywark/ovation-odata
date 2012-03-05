70f31708e6a6b8aabe1f96e477c160f3f1414be3
Ron Reynolds <tequilaron@gmail.com>
2012-03-04 21:31:08 -0800

    added framework and fleshing out test methods - verifying counts by entity type against DB now and started on verifying data in Experiments (timestamps are rounded off and wrong timezone)

--- test/ovation/odata/service/BasicGetTest.java
+++ test/ovation/odata/service/BasicGetTest.java
@@ -1,115 +1,283 @@
 package ovation.odata.service;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
+import junit.framework.Assert;
 import junit.framework.TestCase;
 
 import org.core4j.Enumerable;
 import org.joda.time.DateTime;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.odata4j.consumer.ODataConsumer;
 import org.odata4j.core.EntitySetInfo;
 import org.odata4j.core.OClientBehaviors;
 import org.odata4j.core.OEntity;
+import org.odata4j.core.OEntityGetRequest;
+import org.odata4j.core.OEntityKey;
+import org.odata4j.core.OProperty;
 import org.odata4j.core.OQueryRequest;
-
-import ovation.DataContext;
-import ovation.KeywordTag;
-import ovation.Project;
-import ovation.odata.util.OvationDBTestHelper;
-import ovation.odata.util.OvationDBTestHelper.DeviceData;
-import ovation.odata.util.OvationDBTestHelper.EpochData;
-import ovation.odata.util.OvationDBTestHelper.EpochGroupData;
-import ovation.odata.util.OvationDBTestHelper.ExperimentData;
-import ovation.odata.util.OvationDBTestHelper.ProjectData;
-import ovation.odata.util.OvationDBTestHelper.ResponseData;
-import ovation.odata.util.OvationDBTestHelper.SourceData;
-import ovation.odata.util.OvationDBTestHelper.StimulusData;
-import ovation.odata.util.StringUtils;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 
-public class BasicGetTest extends TestCase {
-	static final String SERVICE_URL 	= "http://localhost:8080/ovodata/Ovodata.svc/";
-	static final String USERNAME 		= "ron";
-	static final String PASSWORD 		= "passpass1";
-	static final String DB_CON_FILE		= "/var/lib/ovation/db/dev.connection";
-	
-	private static final ODataConsumer _odataClient = ODataConsumer.create(SERVICE_URL, OClientBehaviors.basicAuth(USERNAME, PASSWORD)); 
-	
-	private static final Set<String> _knownEntities = Sets.newHashSet(new String[]{
-			"Experiments", "EpochGroups", "_MapEntries", "AnalysisRecords", "Responses", "ExternalDevices",
-			"Sources", "DerivedResponses", "Projects", "URLResources", "_Strings", "Stimuli",
-			"KeywordTags", "Epochs", "Resources"
-	});
+import ovation.AnalysisRecord;
+import ovation.DataContext;
+import ovation.DerivedResponse;
+import ovation.Epoch;
+import ovation.EpochGroup;
+import ovation.Experiment;
+import ovation.ExternalDevice;
+import ovation.KeywordTag;
+import ovation.Project;
+import ovation.Resource;
+import ovation.Response;
+import ovation.Source;
+import ovation.Stimulus;
+import ovation.URLResource;
+import ovation.odata.util.DataContextCache;
+import ovation.odata.util.OvationDBTestHelper;
 
-	// determine all the data in the DB that we're testing against first (direct to DB)
-	private static Project[] _allProjects;
+public class BasicGetTest {
+	static final String SERVICE_URL 	= System.getProperty("ovodataUrl",      "http://localhost:8080/ovodata/Ovodata.svc/");
+	static final String USERNAME 		= System.getProperty("ovationUser", 	"ron");
+	static final String PASSWORD 		= System.getProperty("ovationPassword", "passpass1");
+	static final String DB_CON_FILE		= System.getProperty("ovationDbFile",   "/var/lib/ovation/db/dev.connection");
 	
+	private static ODataConsumer 	_odataClient; 
+	private static DataContext 		_dbContext;
+	
+	@BeforeClass 
+	public static void beforeAllTests() throws Throwable {
+		_odataClient = ODataConsumer.create(SERVICE_URL, OClientBehaviors.basicAuth(USERNAME, PASSWORD)); 
+		_dbContext = OvationDBTestHelper.getContext(USERNAME, PASSWORD, DB_CON_FILE);
+	}
+	
+	@AfterClass 
+	public static void logout() {
+		_odataClient = null; 
+		_dbContext.logout();
+		_dbContext.close();
+		_dbContext = null;
+	 }
+	
+	static enum Entity {
+		EXPERIMENT("Experiments", 			Experiment.class), 
+		EPOCH_GROUP("EpochGroups", 			EpochGroup.class),
+		ANALYSIS_RECORD("AnalysisRecords", 	AnalysisRecord.class), 
+		RESPONSE("Responses", 				Response.class),
+		EXTERNAL_DEVICE("ExternalDevices", 	ExternalDevice.class), 
+		SOURCE("Sources", 					Source.class),
+		DERIVED_RESPONSE("DerivedResponses",DerivedResponse.class), 
+		PROJECT("Projects", 				Project.class),
+		URL_RESOURCE("URLResources", 		URLResource.class), 
+		STIMULUS("Stimuli", 				Stimulus.class),
+		KEYWORD_TAG("KeywordTags", 			KeywordTag.class), 
+		EPOCH("Epochs", 					Epoch.class),
+		RESOURCE("Resources", 				Resource.class),
+		_MAP_ENTRY("_MapEntries", 			Map.Entry.class),
+		_STRING("_Strings",                 String.class),
+		;
+
+		private final String 	_setName;
+		
+		Entity(String setName, Class<?> type) { _setName = setName; }
+	}
+	
+	private static final Set<String> _knownEntities = Sets.newHashSet();
 	static {
-		DataContext context = null;
-		try {
-			context = OvationDBTestHelper.getContext(USERNAME, PASSWORD, DB_CON_FILE);
-			_allProjects = context.getProjects();
-			KeywordTag[] 	keywordTags = context.getAllTags();
-			String[] 		stringTags 	= context.getAllStringTags();
-			String[] 		userNames 	= context.getUsernames();
-			System.out.println("keyword-tags : " + StringUtils.toString(keywordTags));
-			System.out.println("string-tags  : " + StringUtils.toString(stringTags));
-			System.out.println("user-names   : " + StringUtils.toString(userNames));
-		} catch (Exception ex) {
-			ex.printStackTrace();
-			fail("failed to insert test data - " + ex.toString());
-		} finally {
-			if (context != null) {
-				context.close();
-			}
+		for(Entity e : Entity.values()) {
+			_knownEntities.add(e._setName);
 		}
 	}
-//	<collection href="Experiments">
-//	<collection href="EpochGroups">
-//	<collection href="_MapEntries">
-//	<collection href="AnalysisRecords">
-//	<collection href="Responses">
-//	<collection href="ExternalDevices">
-//	<collection href="Sources">
-//	<collection href="DerivedResponses">
-//	<collection href="Projects">
-//	<collection href="URLResources">
-//	<collection href="_Strings">
-//	<collection href="Stimuli">
-//	<collection href="KeywordTags">
-//	<collection href="Epochs">
-//	<collection href="Resources">
-	
+
 	/**
 	 * assert that the server recognizes all entity types we expect it to recognize
 	 */
-	public static void testBasics() {
+	@Test
+	public void testBasics() throws Throwable {
+		// assert that service exposes all known entity types (and no more or less)
 		Enumerable<EntitySetInfo> serviceEntities = _odataClient.getEntitySets();
-		assertEquals(_knownEntities.size(), serviceEntities.count());
+		Assert.assertEquals(_knownEntities.size(), serviceEntities.count());
 		for (EntitySetInfo info : serviceEntities) {
-			assertTrue(_knownEntities.contains(info.getTitle()));
+			Assert.assertTrue(_knownEntities.contains(info.getTitle()));
+		}
+	}
+
+	@Test
+	public void testGetAllExperiments() throws Throwable {
+		List<Experiment> allFromDb = getAllFromDB(Experiment.class);	
+		Enumerable<OEntity> allFromService = getAllEntities(Entity.EXPERIMENT);
+		
+		Assert.assertEquals(allFromDb.size(), allFromService.count());
+
+		for (OEntity entity : allFromService) {
+			compareToDb(Entity.EXPERIMENT, entity);
 		}
 	}
 	
-	/**
-	 * assert that the server returns the same project data as the direct-to-DB connection produced
-	 */
-	public static void testProjectGet() {
+	@Test
+	public void testGetAllEpochGroups() throws Throwable {
+		List<EpochGroup> allFromDb = getAllFromDB(EpochGroup.class);	
+		Enumerable<OEntity> allFromService = getAllEntities(Entity.EPOCH_GROUP);
 
-//			OEntityKey projectId = OEntityKey.create("5cd9ca34-a93b-49ba-a297-3344e74ebbe7");
-		OQueryRequest<OEntity> query = _odataClient.getEntities("Projects");
-		Enumerable<OEntity> projects = query.execute();
-		assertEquals(_allProjects.length, projects.count());
+		// TODO - assert same length and for each in 1 group assert it exists in other group
 		
-		// http://localhost:8080/ovodata/Ovodata.svc/ExternalDevices?$format=json - 6 devices returned
-		// http://localhost:8080/ovodata/Ovodata.svc/Experiments?$format=json - 2
-		// http://localhost:8080/ovodata/Ovodata.svc/Sources?$format=json - 2
-		// http://localhost:8080/ovodata/Ovodata.svc/Projects?$format=json- 2
+	}
+
+
+	@Test
+	public void testGetAllAnalysisRecords() throws Throwable {
+		List<AnalysisRecord> allFromDb = getAllFromDB(AnalysisRecord.class);	
+		Enumerable<OEntity> allFromService = getAllEntities(Entity.ANALYSIS_RECORD);
+
+		// TODO - assert same length and for each in 1 group assert it exists in other group
 		
+	}
+
+	@Test
+	public void testGetAllResponses() throws Throwable {
+		List<Response> allFromDb = getAllFromDB(Response.class);	
+		Enumerable<OEntity> allFromService = getAllEntities(Entity.RESPONSE);
+
+		// TODO - assert same length and for each in 1 group assert it exists in other group
 		
+	}
+
+	@Test
+	public void testGetAllExternalDevices() throws Throwable {
+		List<ExternalDevice> allFromDb = getAllFromDB(ExternalDevice.class);	
+		Enumerable<OEntity> allFromService = getAllEntities(Entity.EXTERNAL_DEVICE);
+
+		// TODO - assert same length and for each in 1 group assert it exists in other group
 		
+	}
+
+	@Test
+	public void testGetAllSources() throws Throwable {
+		List<Source> allFromDb = getAllFromDB(Source.class);	
+		Enumerable<OEntity> allFromService = getAllEntities(Entity.SOURCE);
+
+		// TODO - assert same length and for each in 1 group assert it exists in other group
+		
+	}
+
+	@Test
+	public void testGetAllDerivedResponses() throws Throwable {
+		List<DerivedResponse> allFromDb = getAllFromDB(DerivedResponse.class);	
+		Enumerable<OEntity> allFromService = getAllEntities(Entity.DERIVED_RESPONSE);
+
+		// TODO - assert same length and for each in 1 group assert it exists in other group
+		
+	}
+
+	@Test
+	public void testGetAllProjects() throws Throwable {
+		List<Project> allFromDb = getAllFromDB(Project.class);	
+		Enumerable<OEntity> allFromService = getAllEntities(Entity.PROJECT);
+
+		// TODO - assert same length and for each in 1 group assert it exists in other group
+		
+	}
+
+	@Test
+	public void testGetAllURLResources() throws Throwable {
+		List<URLResource> allFromDb = getAllFromDB(URLResource.class);	
+		Enumerable<OEntity> allFromService = getAllEntities(Entity.URL_RESOURCE);
+
+		// TODO - assert same length and for each in 1 group assert it exists in other group
+		
+	}
+
+	@Test
+	public void testGetAllStimuli() throws Throwable {
+		List<Stimulus> allFromDb = getAllFromDB(Stimulus.class);	
+		Enumerable<OEntity> allFromService = getAllEntities(Entity.STIMULUS);
+
+		// TODO - assert same length and for each in 1 group assert it exists in other group
+		
+	}
+
+	@Test
+	public void testGetAllKeywordTags() throws Throwable {
+		List<KeywordTag> allFromDb = getAllFromDB(KeywordTag.class);	
+		Enumerable<OEntity> allFromService = getAllEntities(Entity.KEYWORD_TAG);
+
+		// TODO - assert same length and for each in 1 group assert it exists in other group
+		
+	}
+
+	@Test
+	public void testGetAllEpochs() throws Throwable {
+		List<Epoch> allFromDb = getAllFromDB(Epoch.class);	
+		Enumerable<OEntity> allFromService = getAllEntities(Entity.EPOCH);
+
+		// TODO - assert same length and for each in 1 group assert it exists in other group
+		
+	}
+
+	@Test
+	public void testGetAllResources() throws Throwable {
+		List<Resource> allFromDb = getAllFromDB(Resource.class);	
+		Enumerable<OEntity> allFromService = getAllEntities(Entity.RESOURCE);
+
+		// TODO - assert same length and for each in 1 group assert it exists in other group
+		
+	}
+
+	
+	static OEntity getEntity(Entity type, String entityId) {
+		OEntityGetRequest<OEntity> req = _odataClient.getEntity(type._setName, OEntityKey.create(entityId));
+		return req.execute();
+	}
+	
+	static Enumerable<OEntity> getAllEntities(Entity type) {
+		OQueryRequest<OEntity> req = _odataClient.getEntities(type._setName);
+		return req.execute();
+	}
+	
+	static <T> List<T> getAllFromDB(Class<T> type) {
+		return Lists.newArrayList(_dbContext.query((Class)type, "true"));
+	}
+	
+	static <T> T getFromDB(String uri) {
+    	return (T)_dbContext.objectWithURI(uri);		
+	}
+	static String getStringProperty(OEntity entity, String name) 		{ return  entity.getProperty(name, String.class).getValue(); }
+	static DateTime getDateTimeProperty(OEntity entity, String name) 	{ return  entity.getProperty(name, DateTime.class).getValue(); }
+	
+	static void compareToDb(Entity type, OEntity entity) {
+		// TODO
+		switch(type) {
+			case EXPERIMENT :
+				String uri = getStringProperty(entity, "URIString");
+				Experiment fromDb = getFromDB(uri);
+				Assert.assertNotNull(fromDb);
+				Assert.assertEquals(fromDb.getNotes(), 				getStringProperty(entity, "Notes"));
+				Assert.assertEquals(fromDb.getSerializedName(), 	getStringProperty(entity, "SerializedName"));
+				Assert.assertEquals(fromDb.getUuid(), 				getStringProperty(entity, "UUID"));
+				Assert.assertEquals(fromDb.getSerializedLocation(), getStringProperty(entity, "SerializedLocation"));
+				Assert.assertEquals(fromDb.getStartTime(), 			getDateTimeProperty(entity, "StartTime"));
+				Assert.assertEquals(fromDb.getEndTime(), 			getDateTimeProperty(entity, "EndTime"));
+				Assert.assertEquals(fromDb.getPurpose(), 			getStringProperty(entity, "Purpose"));
+				//fromDb.getAnnotationGroupTags()... sub-entities
+				break;
+			case ANALYSIS_RECORD :
+			case DERIVED_RESPONSE :
+			case EPOCH :
+			case EPOCH_GROUP :
+			case EXTERNAL_DEVICE :
+			case KEYWORD_TAG :
+			case PROJECT :
+			case RESOURCE :
+			case RESPONSE :
+			case SOURCE :
+			case STIMULUS :
+			case URL_RESOURCE :
+		}
 	}
 }
