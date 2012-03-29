43e620cd668408827cf425632ed3f8ceb4e7d892
Ron Reynolds <tequilaron@gmail.com>
2012-03-28 23:03:38 -0700

    added and then commented out odata4j-0.6 changes to be added back in when we upgrade to 0.6+

--- src/ovation/odata/util/OData4JClientUtils.java
+++ src/ovation/odata/util/OData4JClientUtils.java
@@ -56,7 +56,32 @@ public class OData4JClientUtils {
         
         return count;
     }
-    
+/* FIXME 0.6    
+    private static void reportProperties(Enumerable<EdmProperty> properties){
+        for(EdmProperty property : properties){
+            String p = String.format("Property Name=%s Type=%s Nullable=%s",property.getName(),property.getType(),property.isNullable());
+            if (property.getMaxLength() != null)
+                p = p + " MaxLength="+ property.getMaxLength();
+            if (property.getUnicode() != null)
+                p = p + " Unicode="+ property.getUnicode();
+            if (property.getFixedLength() != null)
+                p = p + " FixedLength="+ property.getFixedLength();   
+            if (property.getStoreGeneratedPattern() != null)
+                p = p + " StoreGeneratedPattern="+ property.getStoreGeneratedPattern();  
+            if (property.getFcTargetPath() != null)
+                p = p + " TargetPath="+ property.getFcTargetPath();
+            if (property.getFcContentKind() != null)
+                p = p + " ContentKind="+ property.getFcContentKind();
+            if (property.getFcKeepInContent() != null)
+                p = p + " KeepInContent="+ property.getFcKeepInContent();
+            if (property.getFcEpmContentKind() != null)
+                p = p + " EpmContentKind="+ property.getFcEpmContentKind();
+            if (property.getFcEpmKeepInContent() != null)
+                p = p + " EpmKeepInContent="+ property.getFcEpmKeepInContent();
+            report("    "+ p);
+        }
+    }
+*/    
     private static void reportProperties(List<EdmProperty> properties){
         for(EdmProperty property : properties){
             String p = String.format("Property Name=%s Type=%s Nullable=%s",property.name,property.type,property.nullable);
@@ -82,7 +107,7 @@ public class OData4JClientUtils {
                 p = p + " EpmKeepInContent="+ property.fcEpmKeepInContent;
             report("    "+ p);
         }
-    }
+    }    
     
     public static void reportMetadata(EdmDataServices services){
         
@@ -140,4 +165,62 @@ public class OData4JClientUtils {
             }
         }
     }
+/* FIXME 0.6    
+    public static void reportMetadata(EdmDataServices services){
+        
+        for(EdmSchema schema : services.getSchemas()){
+            report("Schema Namespace=%s",schema.getNamespace());
+            
+            for(EdmEntityType et : schema.getEntityTypes()){
+                String ets = String.format("  EntityType Name=%s",et.getName());
+                if (et.getHasStream() != null)
+                    ets = ets + " HasStream="+et.getHasStream();
+                report(ets);
+                
+                for(String key : et.getKeys()){
+                    report("    Key PropertyRef Name=%s",key);
+                }
+                
+                reportProperties(et.getProperties());
+                for(EdmNavigationProperty np : et.getNavigationProperties()){
+                    report("    NavigationProperty Name=%s Relationship=%s FromRole=%s ToRole=%s",
+                            np.getName(),np.getRelationship().getFQNamespaceName(),np.getFromRole().getRole(),np.getToRole().getRole());
+                }
+                 
+            }
+            for(EdmComplexType ct : schema.getComplexTypes()){
+                report("  ComplexType Name=%s",ct.getName());
+                
+                reportProperties(ct.getProperties());
+               
+            }
+            for(EdmAssociation assoc : schema.getAssociations()){
+                report("  Association Name=%s",assoc.getName());
+                report("    End Role=%s Type=%s Multiplicity=%s",assoc.getEnd1().getRole(),assoc.getEnd1().getType().getFullyQualifiedTypeName(),assoc.getEnd1().getMultiplicity());
+                report("    End Role=%s Type=%s Multiplicity=%s",assoc.getEnd2().getRole(),assoc.getEnd2().getType().getFullyQualifiedTypeName(),assoc.getEnd2().getMultiplicity());
+            }
+            for(EdmEntityContainer ec : schema.getEntityContainers()){
+                report("  EntityContainer Name=%s IsDefault=%s LazyLoadingEnabled=%s",ec.getName(),ec.isDefault(),ec.getLazyLoadingEnabled());
+                
+                for(EdmEntitySet ees : ec.getEntitySets()){
+                    report("    EntitySet Name=%s EntityType=%s",ees.getName(),ees.getType().getFullyQualifiedTypeName());
+                }
+                
+                for(EdmAssociationSet eas : ec.getAssociationSets()){
+                    report("    AssociationSet Name=%s Association=%s",eas.getName(),eas.getAssociation().getFQNamespaceName());
+                    report("      End Role=%s EntitySet=%s",eas.getEnd1().getRole().getRole(),eas.getEnd1().getEntitySet().getName());
+                    report("      End Role=%s EntitySet=%s",eas.getEnd2().getRole().getRole(),eas.getEnd2().getEntitySet().getName());
+                }
+                
+                for(EdmFunctionImport efi : ec.getFunctionImports()){
+                    report("    FunctionImport Name=%s EntitySet=%s ReturnType=%s HttpMethod=%s",
+                            efi.getName(),efi.getEntitySet()==null?null:efi.getEntitySet().getName(),efi.getReturnType(),efi.getHttpMethod());
+                    for(EdmFunctionParameter efp : efi.getParameters()){
+                        report("      Parameter Name=%s Type=%s Mode=%s",efp.getName(),efp.getType(),efp.getMode());
+                    }
+                }
+            }
+        }
+    }
+*/    
 }
