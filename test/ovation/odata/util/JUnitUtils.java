ba328b4b4db38ce36e4d5dc5768da88c4b882e31
Ron Reynolds <tequilaron@gmail.com>
2012-04-08 16:02:55 -0700

    added String msg to assertEquals and added convience assertEquals for comparing boolean and Boolean

--- test/ovation/odata/util/JUnitUtils.java
+++ test/ovation/odata/util/JUnitUtils.java
@@ -6,15 +6,31 @@ import junit.framework.Assert;
 
 public class JUnitUtils {
 	public static void assertEquals(byte[] expected, byte[] actual) {
-		if (expected == actual) return;	// handles identical arrays and both null
-		Assert.assertEquals(Arrays.toString(expected), Arrays.toString(actual));
+		assertEquals(null, expected, actual);
 	}
+	public static void assertEquals(String msg, byte[] expected, byte[] actual) {
+		if (expected == actual) return;	// handles identical arrays and both null
+		Assert.assertEquals(msg, Arrays.toString(expected), Arrays.toString(actual));
+	}
+	
+	public static void assertEquals(String msg, boolean expected, Boolean actual) {
+		Assert.assertNotNull(msg, actual);
+		Assert.assertEquals(msg, expected, actual.booleanValue());
+	}
+	
 	public static void assertEquals(double[] expected, double[] actual) {
-		if (expected == actual) return;	// handles identical arrays and both null
-		Assert.assertEquals(Arrays.toString(expected), Arrays.toString(actual));
+		assertEquals(null, expected, actual);
 	}
-	public static <T> void assertEquals(T[] expected, T[] actual) {
+	public static void assertEquals(String msg, double[] expected, double[] actual) {
 		if (expected == actual) return;	// handles identical arrays and both null
-		Assert.assertEquals(Arrays.toString(expected), Arrays.toString(actual));
+		Assert.assertEquals(msg, Arrays.toString(expected), Arrays.toString(actual));
+	}
+	
+	public static <T> void assertEquals(T[] expected, T[] actual) {
+		assertEquals(null, expected, actual);
+	}
+	public static <T> void assertEquals(String msg, T[] expected, T[] actual) {
+		if (expected == actual) return;	// handles identical arrays and both null
+		Assert.assertEquals(msg, Arrays.toString(expected), Arrays.toString(actual));
 	}
 }
