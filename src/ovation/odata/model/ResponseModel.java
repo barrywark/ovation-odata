79707930feab5fef63c98fd96022ebaa96a67b17
Ron Reynolds <tequilaron@gmail.com>
2012-05-05 10:33:53 -0700

    removed key type, started StreamingResponse class to adapt Resposne to OAtomStreamEntity - more to work out there - not liking this impl currently

--- src/ovation/odata/model/ResponseModel.java
+++ src/ovation/odata/model/ResponseModel.java
@@ -1,53 +1,101 @@
 package ovation.odata.model;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.HashMap;
 
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.log4j.Logger;
 import org.core4j.Func;
 import org.core4j.Func1;
+import org.odata4j.core.OAtomStreamEntity;
 
+import ovation.IEntityBase;
 import ovation.Response;
+import ovation.odata.model.dao.IEntityBaseAdapter;
+import ovation.odata.util.TypeDataMap;
+import ovation.odata.util.TypeDataMap.TypeData;
 
 import com.google.common.collect.Maps;
 
 /**
- * presents Response data to the OData4J framework
-a * @author Ron
+ * an adapter of Response to OData4J's streaming interface
+ * @author Ron
  */
-public class ResponseModel extends OvationModelBase<String,Response> {
-	static final Logger _log = Logger.getLogger(ResponseModel.class);
+class StreamingResponse extends IEntityBaseAdapter implements OAtomStreamEntity {
+    StreamingResponse(Response res) { super(res); }
+    /**
+     * @return ??
+     */
+    @Override
+    public String getAtomEntitySource() {
+        ResponseModel._log.info("getAtomEntitySource() called from ", new Throwable());
+        return null;
+    }
+    /**
+     * @return ??
+     */
+    @Override
+    public String getAtomEntityType() {
+        ResponseModel._log.info("getAtomEntityType() called from ", new Throwable());
+        return null;
+    }
+}
 
-	static final HashMap<String,Class<?>> _propertyTypeMap 	 = Maps.newHashMap();
-	static final HashMap<String,Class<?>> _collectionTypeMap = Maps.newHashMap();
-	
-	static { addResponse(_propertyTypeMap, _collectionTypeMap); }
-	
-	public ResponseModel() 					{ super(_propertyTypeMap, _collectionTypeMap); }
-	public String			getTypeName()	{ return "Response"; }
-	public String 			entityName() 	{ return "Responses"; }
-	public Class<Response> 	getEntityType() { return Response.class; }
-	public Class<String> 	getKeyType() 	{ return String.class; }
-	
-	public Iterable<?> 	getCollectionValue	(Object target, String collectionName)	{ return getCollection((Response)target, CollectionName.valueOf(collectionName)); }
-	public Object 		getPropertyValue	(Object target, String propertyName) 	{ return getProperty((Response)target,   PropertyName.valueOf(propertyName)); }
-	
-	public Func<Iterable<Response>> allGetter() {
-		return new Func<Iterable<Response>>() { 
-			public Iterable<Response> apply() { 
-				final Iterable<Response> queryIter = executeQueryInfo();
-				if (queryIter != null) {
-					return queryIter;
-				}
-				return executeQuery(GET_ALL_PQL); 
-			} 
-		};
-	}
+/**
+ * presents Response data to the OData4J framework
+ * @author Ron
+ */
+public class ResponseModel extends OvationModelBase<Response> {
+    static final Logger _log = Logger.getLogger(ResponseModel.class);
 
-	public Func1<Response,String> idGetter() {
-		return new Func1<Response,String>() {
-			public String apply(Response record) {
-				return record.getUuid();
-			}		
-		};
-	}
+    static final HashMap<String,Class<?>> _propertyTypeMap      = Maps.newHashMap();
+    static final HashMap<String,Class<?>> _collectionTypeMap = Maps.newHashMap();
+    
+    static { addResponse(_propertyTypeMap, _collectionTypeMap); }
+    
+    public ResponseModel()                  { super(_propertyTypeMap, _collectionTypeMap); }
+    public String           getTypeName()   { return "Response"; }
+    public String           entityName()    { return "Responses"; }
+    public Class<Response>  getEntityType() { return Response.class; }
+    
+    public Iterable<?>  getCollectionValue(Object target, String collectionName){ return getCollection((Response)target, CollectionName.valueOf(collectionName)); }
+    public Object       getPropertyValue(Object target, String propertyName)    { return getProperty((Response)target,   PropertyName.valueOf(propertyName)); }
+    
+    // TODO - this won't work since it'll expect the entity instance to implement this interface
+    public OAtomStreamEntity getStreamHandler(final Response res) {
+        return new OAtomStreamEntity() {
+            @Override public String getAtomEntityType() {  
+                TypeData typeData = TypeDataMap.getUTIData(res.getUTI());
+                return typeData != null ? typeData.getMimeType() : null;
+            }
+            @Override public String getAtomEntitySource() {
+                try {
+                    return "/ovodata/media/?uri=" + URLEncoder.encode(res.getURIString(), "US-ASCII");
+                } catch (UnsupportedEncodingException e) {
+                    throw new Error("US-ASCII encoding not supported - something is terribly wrong");
+                }
+            }
+        };
+    }
+    public Func<Iterable<Response>> allGetter() {
+        return new Func<Iterable<Response>>() { 
+            public Iterable<Response> apply() { 
+                final Iterable<Response> queryIter = executeQueryInfo();
+                if (queryIter != null) {
+                    // wrap Responses in StreamingResponse objects
+                    return queryIter;
+                }
+                return executeQuery(GET_ALL_PQL); 
+            } 
+        };
+    }
+
+    public Func1<Response,String> idGetter() {
+        return new Func1<Response,String>() {
+            public String apply(Response record) {
+                return record.getUuid();
+            }        
+        };
+    }
 }
